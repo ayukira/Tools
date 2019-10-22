915517f45f14dff1960b677acbc3f1325648ba75
kira <yzm520@live.cn>
2019-10-22 17:44:44 +0800

    注册服务器

--- AutofacTool/AutofacTool/IocTest.cs
+++ AutofacTool/AutofacTool/IocTest.cs
@@ -16,11 +16,13 @@ namespace AutofacTool
         {
             using (var scope = Autofac.Container.BeginLifetimeScope())
             {
+                //如果你不清楚一个服务是否被注册了, 你可以通过 ResolveOptional() 或 TryResolve() 尝试解析: TryResolve暂未找到传参
                 _database = scope.ResolveOptional<IDataBase>(new NamedParameter("type", "Test_"));
-                if (scope.TryResolve<IDataBase>(out _database))
-                {
-                    // Do something with the resolved provider value.
-                }
+
+                //if (scope.TryResolve<IDataBase>(out _database))
+                //{
+                //    // Do something with the resolved provider value.
+                //}
 
             }
         }
