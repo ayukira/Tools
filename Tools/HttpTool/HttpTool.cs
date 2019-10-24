673ea9f49365a625165812e2351b737d70d2334f
kira <yzm520@live.cn>
2019-10-24 16:11:14 +0800

    增加数组扩展操作

--- Tools/HttpTool/HttpTool.cs
+++ Tools/HttpTool/HttpTool.cs
@@ -3,34 +3,52 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Tools.HttpTool
+namespace Tools
 {
+    /// <summary>
+    /// 可创建HttpClinet的FactoryTool
+    /// </summary>
     public class HttpTool
     {
         private static IHttpClientFactory _httpClientFactory;
         private static Lazy<HttpTool> instance = new Lazy<HttpTool>(() => { return new HttpTool(); }, true);
-
+        /// <summary>
+        /// HttpClinetFactory单例
+        /// </summary>
         public static HttpTool Instance { get { return instance.Value; } }
         public static bool IsCreate { get { return instance.IsValueCreated; } }
         private HttpTool()
         {
             _httpClientFactory = new ServiceCollection().AddHttpClient().BuildServiceProvider().GetService<IHttpClientFactory>();
         }
-
+        /// <summary>
+        /// 简单异步Get请求
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
         public static async Task<string> GetAsync(string uri)
         {
             HttpClient httpClient = _httpClientFactory.CreateClient();
             var response = await httpClient.GetAsync(uri);
             return await response.Content.ReadAsStringAsync();
         }
-
+        /// <summary>
+        /// 简单异步Post请求
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="httpContent">请求Content</param>
+        /// <returns></returns>
         public static async Task<string> PostAsync(string uri, HttpContent httpContent)
         {
             HttpClient httpClient = _httpClientFactory.CreateClient();
             var response = await httpClient.PostAsync(uri, httpContent);
             return await response.Content.ReadAsStringAsync();
         }
-
+        /// <summary>
+        /// 获得一个HttpClient
+        /// </summary>
+        /// <param name="name">ClinetName</param>
+        /// <returns></returns>
         public static HttpClient GetHttpClient(string name="")
         {
             if(string.IsNullOrWhiteSpace(name))
